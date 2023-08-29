bcfd47016bc0037134278cf266b53f0e3d5cec62
yap cheah shen <yapcheahshen@gmail.com>
2023-08-29 09:43:18 +0800

    add off

--- build-dict.cmd
+++ build-dict.cmd
@@ -1,10 +1,10 @@
-cmd/c ptk dump dc dc.off
-cmd/c ptk listwords dc.off zhwiki-20230701-pages-articles-multistream-index.txt
-copy/Y dc.off-listwords.csv dc-wikipedia.csv
-cmd/c ptk listwords dc.off fgdcd-entries.txt
-copy/Y dc.off-listwords.csv dc-fgdzd.csv
-cmd/c ptk listwords dc.off dfb-entries.txt
-copy/Y dc.off-listwords.csv dc-dfb.csv
+cmd/c ptk dump ylz ylz.off
+cmd/c ptk listwords ylz.off zhwiki-20230701-pages-articles-multistream-index.txt
+copy/Y ylz.off-listwords.csv ylz-wikipedia.csv
+cmd/c ptk listwords ylz.off fgdcd-entries.txt
+copy/Y ylz.off-listwords.csv ylz-fgdzd.csv
+cmd/c ptk listwords ylz.off dfb-entries.txt
+copy/Y ylz.off-listwords.csv ylz-dfb.csv
 node gen-dictentry.js
 
 copy/Y off\zentries.tsv ..\dharmacloud\off\
